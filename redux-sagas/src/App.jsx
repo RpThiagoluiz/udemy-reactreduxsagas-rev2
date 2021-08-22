9fe37d2f9cee6ed5adce8ceb56d7c66d5e4783ce
thiago <rp.thiagoluiz@gmail.com>
2021-08-22 05:50:31 -0300

    feat: test:

--- redux-sagas/src/App.jsx
+++ redux-sagas/src/App.jsx
@@ -5,6 +5,8 @@ import {
   Segment,
   Statistic,
   Icon,
+  Form,
+  Button,
 } from 'semantic-ui-react';
 import './App.css';
 
@@ -89,5 +91,28 @@ export const App = () => (
         </Grid.Row>
       </Grid>
     </Segment>
+
+    <Header as="h3">Add new transaction</Header>
+    <Form unstackable>
+      <Form.Group>
+        <Form.Input
+          icon="tags"
+          width={12}
+          label="Description"
+          placeholder="New shinny thing"
+        />
+        <Form.Input
+          icon="dollar"
+          width={12}
+          label="Value"
+          placeholder="100.00"
+        />
+      </Form.Group>
+      <Button.Group style={{ marginTop: 20 }}>
+        <Button color="red">Cancel</Button>
+        <Button.Or />
+        <Button primary>Ok</Button>
+      </Button.Group>
+    </Form>
   </Container>
 );
